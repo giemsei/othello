cb580e50f386818175416400540f00c095adc038
gianmarioale <giammi.alessio@gmail.com>
2021-04-02 09:17:30 +0200

    prototipo index

--- js/index.js
+++ js/index.js
@@ -1,4 +1,36 @@
 //punto di partenza dell'applicativo p5.js
 
-function setup(){}
-function draw(){}
+var o = {
+    CW:0,
+    CH:0,
+}
+
+function init(){
+    o.CW = windowWidth;
+    o.CH = windowHeight;
+}
+function windowResized() {
+    init();
+    resizeCanvas(o.CW, o.CH)
+}
+function setup(){
+    init();
+    createCanvas(o.CW, o.CH);
+    gioco = new Gioco();
+}
+function draw(){
+    background("whitesmoke");
+    gioco.draw()
+}
+function preload(){
+    o.sprite = loadImage("./img/pedine.png", (img) => {
+        o.spritex = img.width / 8;
+        o.spritey = img.height / 4;
+    })
+}
+function mousePressed(){}
+function mouseReleased(){}
+function keyPressed(){}
+
+
+
